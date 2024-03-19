dc60c196cc7c202ad0506bcb3e0e1f38c18c1934
Danchkin_Sab <69789792+DanilaProgrammist@users.noreply.github.com>
2024-03-19 09:58:46 +0300

    Add files via upload

---
+++ test.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "button.h"
+
+int main(void){
+	ButtInit(&btn, Button_Pin, Button_GPIO_Port, CLICK);
+	while(1){
+		if(ButtTrigg(&btn) == 1){
+			//do something
+		}
+	}
+	return 1;
+}
